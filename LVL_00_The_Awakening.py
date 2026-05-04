6e6f1712bd76af02c1c695291bb591b858da2d1a
mahimababariya <mahimababariya08@gmail.com>
2026-05-04 19:58:08 +0530

    Create LVL_00_The_Awakening.py

---
+++ LVL_00_The_Awakening.py
@@ -0,0 +1,6 @@
+# S01E01 - The Awakening
+# DEV NOTES: 
+# - I learned that print() is the voice and input() is the ear.
+
+player_name = input("Who goes there? ")
+print("Welcome to the server, " + player_name + "!")
